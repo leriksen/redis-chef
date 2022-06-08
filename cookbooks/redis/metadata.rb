973edeb2b3692f71c6c5f068fd58cea01bbbd8af
Leif Eriksen <leif.eriksen.au@gmail.com>
2022-06-08 10:52:58 +1000

    add uninstall recipe

--- cookbooks/redis/metadata.rb
+++ cookbooks/redis/metadata.rb
@@ -4,9 +4,10 @@ maintainer_email 'leif.eriksen.au+redis-chef-github@gmail.com'
 license 'All Rights Reserved'
 description 'Installs/Configures Redis daemon'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.3.0'
+version '0.3.2'
 chef_version '>= 16.0'
 source_url 'https://github.com/leriksen/redis-chef'
 issues_url 'https://github.com/leriksen/redis-chef/issues'
 
 recipe 'redis::default', 'Install and configure Redis daemon'
+recipe 'redis::uninstall-redis', 'Uninstalls Redis daemon'
