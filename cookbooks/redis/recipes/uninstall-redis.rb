e66c870636a64829fde2178a21e185126f300b8b
Leif Eriksen <leif.eriksen.au@gmail.com>
2022-06-08 14:47:22 +1000

    Update uninstall-redis.rb

--- cookbooks/redis/recipes/uninstall-redis.rb
+++ cookbooks/redis/recipes/uninstall-redis.rb
@@ -9,10 +9,12 @@ end
 
 file File.join(node['redis']['conf_dir'], 'redis.conf') do
   action :delete
-  only_if { File.exist? File.join(node['redis']['conf_dir'], 'redis.conf') }
+  only_if { ::File.exist? File.join(node['redis']['conf_dir'], 'redis.conf') }
 end
 
 file File.join(node['redis']['sysconfig_dir'], node['redis']['pkg_name']) do
   action :delete
-  only_if { File.exist? File.join(node['redis']['sysconfig_dir'], node['redis']['pkg_name']) }
+  only_if { ::File.exist? File.join(node['redis']['sysconfig_dir'], node['redis']['pkg_name']) }
 end
+
+# we leave the redis logs in /var/log/redis for post-mortems
